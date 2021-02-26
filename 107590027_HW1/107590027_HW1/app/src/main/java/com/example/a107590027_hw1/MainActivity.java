1f90dc646cc6aaa7f30d58451bc50923250f0547
tess8901 <79504911+tess8901@users.noreply.github.com>
2021-02-26 16:39:35 +0800

    107590027_HW1

---
+++ 107590027_HW1/107590027_HW1/app/src/main/java/com/example/a107590027_hw1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a107590027_hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "MainActivity layout is complete");
+    }
+}
